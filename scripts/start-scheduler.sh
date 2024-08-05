b99e16a70867a02ebab2f0914258091a22bdda9b
Vasilis Raptakis <scaraveos@gmail.com>
2024-08-05 22:52:25 +0000

    Minor dependency updates

--- scripts/start-scheduler.sh
+++ scripts/start-scheduler.sh
@@ -23,6 +23,6 @@ export SZ_DB_PASS
 
 # Start the services using docker-compose
 echo "Starting services with docker-compose"
-docker compose up -d
+docker compose up --build -d
 
 echo "Scheduler services started successfully"
