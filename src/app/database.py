9c22cdcc50dfd1a57475a5a47abc92fcd2de4d5a
Vasilis Raptakis <scaraveos@gmail.com>
2024-09-12 11:25:19 -0700

    Fix query

--- src/app/database.py
+++ src/app/database.py
@@ -66,7 +66,7 @@ class Database:
                     stopped_timestamp TIMESTAMP WITH TIME ZONE,
                     completed_timestamp TIMESTAMP WITH TIME ZONE,
                     failed_timestamp TIMESTAMP WITH TIME ZONE,
-                    CONSTRAINT jobs_pk PRIMARY KEY (job_id, user_id)
+                    CONSTRAINT job_status_timestamps_pk UNIQUE (job_id, user_id)
                 )
             ''')
             logger.info("Database tables created")
