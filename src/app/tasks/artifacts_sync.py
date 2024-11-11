37aeb430e854ae40f8c04ec30f339b541da57a0b
Vasilis Raptakis <scaraveos@gmail.com>
2024-11-11 15:22:27 -0800

    Exception handling in background task

--- src/app/tasks/artifacts_sync.py
+++ src/app/tasks/artifacts_sync.py
@@ -16,18 +16,21 @@ async def sync_job_artifacts(db: Database):
     """
     logger.info("Starting artifacts sync")
 
-    # Get all jobs that are not in terminal status or are recently completed jobs
-    lum_jobs = await db.get_jobs_by_status_lum(NON_TERMINAL_JOB_STATUSES)
-    gcp_jobs = await db.get_jobs_by_status_gcp(NON_TERMINAL_JOB_STATUSES)
-    recently_completed_jobs = await db.get_recently_completed_jobs()
-    all_jobs = lum_jobs + gcp_jobs + recently_completed_jobs
+    try:
+        # Get all jobs that are not in terminal status or are recently completed jobs
+        lum_jobs = await db.get_jobs_by_status_lum(NON_TERMINAL_JOB_STATUSES)
+        gcp_jobs = await db.get_jobs_by_status_gcp(NON_TERMINAL_JOB_STATUSES)
+        recently_completed_jobs = await db.get_recently_completed_jobs()
+        all_jobs = lum_jobs + gcp_jobs + recently_completed_jobs
 
-    # Initialize GCS client
-    gcs_client = storage.Client(project=config.gcp_project)
+        # Initialize GCS client
+        gcs_client = storage.Client(project=config.gcp_project)
 
-    # Pull artifacts in parallel
-    results = await asyncio.gather(
-        *[pull_artifacts_meta_from_gcs(job['job_id'], job['user_id'], db, gcs_client) for job in all_jobs])
+        # Pull artifacts in parallel
+        results = await asyncio.gather(
+            *[pull_artifacts_meta_from_gcs(job['job_id'], job['user_id'], db, gcs_client) for job in all_jobs])
 
-    # Update artifacts in DB in parallel
-    await asyncio.gather(*[db.update_job_artifacts(*result) for result in results if result])
+        # Update artifacts in DB in parallel
+        await asyncio.gather(*[db.update_job_artifacts(*result) for result in results if result])
+    except Exception as e:
+        logger.error(f"Error in artifacts sync: {str(e)}")
